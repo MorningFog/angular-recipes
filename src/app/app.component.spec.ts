67265cc537d47e34185a3a96e77eb1bca8646d15
David Michael <davidmichael@Kate-Bush.local>
2019-11-28 10:47:23 +0000

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'recipe-shop-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('recipe-shop-app');
+    expect(app.title).toEqual('recipes-shop-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('recipe-shop-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('recipes-shop-app app is running!');
   });
 });
