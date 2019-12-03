81e32b4d93898fc51a264c3d47c06926e73f0673
MorningFog <veronag@me.com>
2019-12-03 09:40:15 +0000

    Small changes to tidy up before next assignment.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Chicken Wings', 'Crispy yet succulent', 'https://cdn.pixabay.com/photo/2019/05/19/12/23/chicken-farm-4213949_1280.jpg'),
+    new Recipe('Chicken Wings', 'Crispy yet succulent', 'https://get.pxhere.com/photo/japanese-food-chicken-wings-chopsticks-deboned' +
+      '-pork-gyoza-dish-cuisine-appetizer-seafood-asian-food-animal-source-foods-meal-side-dish-fried-food-lunch-shrimp-recipe-' +
+      'japanese-cuisine-1418346.jpg'),
     new Recipe('Avocado on Toast', 'Healthy alternative', 'https://p0.pxfuel.com/preview/359/525/312/avocado-toast-snack-healthy.jpg'),
     new Recipe('Healthy Breakfast', 'Start your day well', 'https://p0.piqsels.com/preview/540/349/270/bread-breakfast-dish-green.jpg'),
   ];
@@ -16,5 +18,4 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
